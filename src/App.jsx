fcaa21ffd0b6e8c4dd48207addec4928adda4fe4
Vishu-12 <garg.vishu810@gmail.com>
2024-07-31 07:55:50 +0530

    complete game tic tac toe

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TicTacToe from "./components/TicTacToe/TicTacToe";
+
+export default function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
